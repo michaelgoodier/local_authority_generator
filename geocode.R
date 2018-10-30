1e4d7e38e100b2b6746c015b9bb273df0109f65c
michaelgoodier <michaelgoodier7@gmail.com>
2018-10-30 12:14:59 +0000

    Update geocode.R

--- geocode.R
+++ geocode.R
@@ -1,9 +1,10 @@
 #LOCAL AUTHORITY AND NEWSROOM GENERATOR
 #This takes badly formatted location data and finds Local Authority Name
 #and also attaches a column with relevant Reach PLC newsroom.
+#put in folder with newsrooms.csv and the shapefiles
 #Last Updated: 29/10/18 Version: 1.0 By: Michael Goodier
 
-# Setup -------------------------------------------------------------------
+# 0 Setup -------------------------------------------------------------------
 
 #Libraries
 library(ggmap)
@@ -14,22 +15,24 @@ library(readr)
 library(dplyr)
 library(rgdal)
 library(spdplyr)
+library(tidyr)
 
-# Add Google Geocode API key (may want to register your own so I don't run out of free credit)
-register_google(key = "YOUR GOOGLE KEY", account_type = "premium")
+# Add Google Geocode API key (may want to register your own https://developers.google.com/maps/documentation/geocoding/get-api-key)
+register_google(key = "YOUR_API_KEY", account_type = "premium")
 
 #load csv with locations you want coded (change "folder" to the folder you are working from which contains shapefiles + csv)
-setwd("folder")
-dataframe <- read_csv("dataframe.csv") #change "dataframe.csv" to your csv containg locations
+setwd("YOUR FOLDER")
+dataframe <- read_csv("YOUR CSV.csv") #change "YOUR CSV.csv" to your csv containg location data
 
 #cleaning
-names(dataframe)[names(dataframe) == 'old name'] <- 'locations' # Used to rename column of choice ("old name") to "locations"
+names(dataframe)[names(dataframe) == 'OLD COLUMN NAME'] <- 'locations' # Used to rename column of choice ("old name") to "locations"
 #dataframe[] <- lapply(dataframe, gsub, pattern='[^ -~]', replacement='') # Used to strip out non-utf characters (if needed)
 
 #load LA to newsroom lookup
 newsrooms <- read_csv("newsrooms.csv")
 
-# Geocoding ----------------------------------------------------------------
+# 1 Geocoding ----------------------------------------------------------------
+#if you already have lat/lon, skip to step 2
 
 #dataframe is your dataframe, locations is the name of the column with your locations in
 #you can only geocode up to 2500 in one go afaik
@@ -45,15 +48,19 @@ geocoded <- geocode(dataframe$locationsuk, output = "latlona", source = "google"
 #merge our results with initial dataframe
 geocoded <- bind_cols(dataframe, geocoded)
 
-# Load shapefile of Local Authority Districts ------------------------------
+# 2 Load shapefile of Local Authority Districts ------------------------------
+
+#skipping straight here as already have lat/lon columns? do:
+#geocoded <- dataframe
+#geocoded <- drop_na(geocoded, lon) #remove any NAs from lat/lon
 
 #Shapefile downloaded from here: http://geoportal.statistics.gov.uk/datasets/local-authority-districts-december-2017-full-clipped-boundaries-in-great-britain
 #TO DO: Add in Northern Ireland
 
-#read UK LA Shapefile (from current working directory)
+#read UK LA Shapefile (from current working directory) - takes a bit of time as large
 uk.shp <- readOGR(dsn = ".", layer = "Local_Authority_Districts_December_2017_Full_Clipped_Boundaries_in_Great_Britain")
  
-# Convert to longitude / latitude with WGS84 Coordinate System
+# Convert to longitude / latitude with WGS84 Coordinate System - again takes a bit of time
 wgs84 = '+proj=longlat +datum=WGS84'
 uk.shp_trans <- spTransform(uk.shp, CRS(wgs84))
 
@@ -63,9 +70,9 @@ locations.sp <- geocoded %>% select(lon,lat)
 coordinates(locations.sp) <- ~lon+lat
 proj4string(locations.sp) <- CRS(wgs84) #sets co-ordinate system
 
-# Find LA and merge --------------------------------------------------------
+# 3 Find LA and merge --------------------------------------------------------
 
-#take LA data from uk shapefile and attach to points within LA
+#take LA data from uk shapefile and attach to points within LA (takes a bit of time)
 extracted.data <- over(locations.sp, uk.shp_trans)
 
 #merge our results with initial dataframe
@@ -74,5 +81,5 @@ geocoded$LA <- extracted.data$lad17nm
 #add newsroom to dataframe
 geocoded <- left_join(geocoded, newsrooms, by = "LA")
 
-#export csv file 
+#export you new csv file 
 write_csv(geocoded, "geocoded.csv")
